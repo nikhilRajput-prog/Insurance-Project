a35c8563421cd8d9f7ab491ff275ec2e76c0e60c
Nikhil <rajput.nikhil1418@gmail.com>
2026-08-13 11:12:52 +0530

    logging and exception added

---
+++ demo.py
@@ -0,0 +1,18 @@
+# below code is to check the logging info
+# from src.logger import logging
+# logging.debug("Starting the demo.py script...")
+# logging.info("This is an info message from demo.py")
+# logging.warning("This is a warning message from demo.py")   
+# logging.error("This is an error message from demo.py")  
+# logging.error("This is a critical message from demo.py")
+
+# # below code is to check the exception config
+# from src.logger import logging
+# from src.exception import MyException
+# import sys
+
+# try:
+#     a = 1+'Z'
+# except Exception as e:
+#     logging.info(e)
+#     raise MyException(e, sys) from e
