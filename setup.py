eee6d057bd3958dba624d9b7c8578e132a328b30
Nikhil <rajput.nikhil1418@gmail.com>
2026-08-13 10:08:42 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Nikhil Rajput",
+    author_email="rajput.nikhil1418@gmail.com",
+    packages=find_packages()
+)
