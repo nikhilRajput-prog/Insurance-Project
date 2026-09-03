eaaed8a630278f61fc4df50c52491fc446fd5f90
Nikhil <rajput.nikhil1418@gmail.com>
2026-09-03 14:46:01 +0530

    Model evaluatin and model pusher to S3 done

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -70,7 +70,7 @@ MIN_SAMPLES_SPLIT_RANDOM_STATE: int = 101
 MODEL Evaluation related constants
 """
 MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
-MODEL_BUCKET_NAME = "my-model-mlopsproj"
+MODEL_BUCKET_NAME = "rajput-nikhil-project"
 MODEL_PUSHER_S3_KEY = "model-registry"
 
 
