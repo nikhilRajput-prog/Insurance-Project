b54bf620712d503c265cc13b4e0314f18e5070d0
Nikhil <rajput.nikhil1418@gmail.com>
2026-09-03 15:12:05 +0530

    app ready locally

--- app.py
+++ app.py
@@ -79,7 +79,10 @@ async def index(request: Request):
     Renders the main HTML form page for vehicle data input.
     """
     return templates.TemplateResponse(
-            "vehicledata.html",{"request": request, "context": "Rendering"})
+    request=request,
+    name="vehicledata.html",
+    context={"context": "Rendering"}
+)
 
 # Route to trigger the model training process
 @app.get("/train")
@@ -133,8 +136,9 @@ async def predictRouteClient(request: Request):
 
         # Render the same HTML page with the prediction result
         return templates.TemplateResponse(
-            "vehicledata.html",
-            {"request": request, "context": status},
+            request=request,
+            name="vehicledata.html",
+            context={"request": request, "context": status},
         )
         
     except Exception as e:
